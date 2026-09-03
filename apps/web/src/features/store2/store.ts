7b8456d3f86ba9961dfeb924b7cdc6de693e93b2
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2026-09-03 10:11:38 -0400

    ongoing work

--- apps/web/src/features/store2/store.ts
+++ apps/web/src/features/store2/store.ts
@@ -1,353 +1,331 @@
 import {
-  fromInit,
-  isFunction,
-  isReset,
-  type SetStateWithReset,
+	fromInit,
+	isFunction,
+	isReset,
+	type SetStateWithReset,
 } from '../store/functions'
 import { sortedPush, sortedRemove } from './utils'
 
 let nextIndex = 0
 
 function call(cb: () => void) {
-  cb()
+	cb()
 }
 
 function exhaustive(n: never): never {
-  throw new Error(`unexpected value ${n}`)
+	throw new Error(`unexpected value ${n}`)
 }
 
 type Instance<S> = {
-  symbol: AtomSymbol<any, any>
-  dirty: boolean
-  value: S
-  effectValue: S | undefined
-  effectCleanup: (() => void) | undefined
-  deps: Instance<any>[]
-  subs: Instance<any>[]
-  subscriptions: Set<() => void>
-  mounted: boolean
+	symbol: AtomSymbol<any, any>
+	dirty: boolean
+	value: S
+	effectValue: S | undefined
+	effectCleanup: (() => void) | undefined
+	deps: Instance<any>[]
+	subs: Instance<any>[]
+	subscriptions: Set<() => void>
+	mounted: boolean
 }
 
 type CoreSymbol<S, E> = {
-  index: number
-  effect?: Effect<S, E>
+	index: number
+	effect?: Effect<S, E>
 }
 
 type PrimitiveSymbol<S, E> = CoreSymbol<S, E> & {
-  type: 'primitive'
-  getter: (() => S) | S
-  readonly event?: E
+	type: 'primitive'
+	getter: (() => S) | S
+	readonly event?: E
 }
 
 type DerivedSymbol<S, E> = CoreSymbol<S, E> & {
-  type: 'derived'
-  getter: Getter<S>
-  setter: Setter<E>
+	type: 'derived'
+	getter: Getter<S>
+	setter: Setter<E>
 }
 
 type AtomSymbol<S, E> = PrimitiveSymbol<S, E> | DerivedSymbol<S, E>
 type AtomFamily<S, Key, E> = {
-  type: 'family'
-  create: (key: Key) => AtomSymbol<S, E>
+	type: 'family'
+	create: (key: Key) => AtomSymbol<S, E>
 }
 export type AtomLike<S, Key, E> = AtomSymbol<S, E> | AtomFamily<S, Key, E>
 
 type Read = {
-  <S, Key, E>(symbol: AtomFamily<S, Key, E>, key: Key): S
-  <S, E>(symbol: AtomSymbol<S, E>): S
+	<S, Key, E>(symbol: AtomFamily<S, Key, E>, key: Key): S
+	<S, E>(symbol: AtomSymbol<S, E>): S
 }
 type Write = {
-  <S, Key, E>(symbol: AtomFamily<S, Key, E>, key: Key, event: E): void
-  <S, E>(symbol: AtomSymbol<S, E>, event: E): void
+	<S, Key, E>(symbol: AtomFamily<S, Key, E>, key: Key, event: E): void
+	<S, E>(symbol: AtomSymbol<S, E>, event: E): void
 }
 type Getter<S> = (read: Read) => S
 type Setter<E> = (read: Read, write: Write, e: E) => void
 type Effect<S, E> = (
-  next: S | undefined,
-  last: S | undefined,
-  send: (event: E) => void,
+	next: S | undefined,
+	last: S | undefined,
+	send: (event: E) => void,
 ) => void | (() => void)
 
 export function primitive<S>(
-  getter: S | (() => S),
-  effect?: Effect<NoInfer<S>, SetStateWithReset<S>>,
+	getter: S | (() => S),
+	effect?: Effect<NoInfer<S>, SetStateWithReset<S>>,
 ): AtomSymbol<S, SetStateWithReset<S>> {
-  return {
-    type: 'primitive' as const,
-    getter,
-    effect,
-    index: nextIndex++,
-  }
+	return {
+		type: 'primitive' as const,
+		getter,
+		effect,
+		index: nextIndex++,
+	}
 }
 
 export function derived<S, E>(
-  getter: Getter<S>,
-  setter: Setter<E>,
-  effect?: Effect<NoInfer<S>, E>,
+	getter: Getter<S>,
+	setter: Setter<E>,
+	effect?: Effect<NoInfer<S>, E>,
 ): AtomSymbol<S, E> {
-  return {
-    type: 'derived' as const,
-    getter,
-    setter,
-    effect,
-    index: nextIndex++,
-  }
+	return {
+		type: 'derived' as const,
+		getter,
+		setter,
+		effect,
+		index: nextIndex++,
+	}
 }
 
 export function family<S, K, E>(
-  create: (key: K) => AtomSymbol<S, E>,
+	create: (key: K) => AtomSymbol<S, E>,
 ): AtomFamily<S, K, E> {
-  return {
-    type: 'family' as const,
-    create,
-  }
+	return {
+		type: 'family' as const,
+		create,
+	}
 }
 
 type Tasks = {
-  primitives: Map<Instance<any>, any>
-  effects: Set<Instance<any>>
+	primitives: Map<Instance<any>, any>
+	effects: Set<Instance<any>>
 }
 
 export class Store {
-  private contents = new WeakMap<any, any>()
-  private tasks: Tasks | undefined = undefined
-  private flushingEffects: Set<Instance<any>> | undefined
-  constructor() { }
-  private getTasks(): Tasks {
-    if (this.tasks) return this.tasks
-    this.tasks = {
-      primitives: new Map(),
-      effects: new Set(),
-    }
-    queueMicrotask(this.flush.bind(this))
-    return this.tasks
-  }
-  private getInstance<S, Key, E>(
-    symbol: AtomFamily<S, Key, E>,
-    key: Key,
-  ): Instance<S>
-  private getInstance<S, E>(symbol: AtomSymbol<S, E>): Instance<S>
-  private getInstance<S, Key, E>(
-    symbol: AtomFamily<S, Key, E> | AtomSymbol<S, E>,
-    key?: Key,
-  ): Instance<S> {
-    if (symbol.type === 'family') {
-      let entry = this.contents.get(symbol)
-      if (entry === undefined) {
-        entry = new Map()
-        this.contents.set(symbol, entry)
-      }
-      let res = entry.get(key)
-      if (res === undefined) {
-        res = this.getInstance(symbol.create(key!))
-        entry.set(key, res)
-      }
-      return res
-    }
-    let res = this.contents.get(symbol)
-    if (res === undefined) {
-      res = this.createInstance(symbol)
-      this.contents.set(symbol, res)
-    }
-    return res
-  }
-  private createInstance<S, E>(symbol: AtomSymbol<S, E>): Instance<S> {
-    return {
-      symbol,
-      dirty: symbol.type === 'derived',
-      value:
-        symbol.type === 'primitive'
-          ? fromInit(symbol.getter)
-          : (undefined as any),
-      effectValue: undefined,
-      effectCleanup: undefined,
-      deps: [],
-      subs: [],
-      subscriptions: new Set(),
-      mounted: false,
-    }
-  }
-  private enqueueEffect<S>(instance: Instance<S>) {
-    if (!instance.symbol.effect) return
-    if (this.flushingEffects) {
-      this.flushingEffects.add(instance)
-      return
-    }
-    this.getTasks().effects.add(instance)
-  }
-  private notify<S>(instance: Instance<S>) {
-    if (instance.symbol.type === 'derived') instance.dirty = true
-    if (instance.mounted) this.enqueueEffect(instance)
-    for (const sub of instance.subs) this.notify(sub)
-    instance.subscriptions.forEach(call)
-  }
-  private isMounted<S>(instance: Instance<S>) {
-    return (
-      instance.subscriptions.size > 0 ||
-      (instance.symbol.type === 'derived' &&
-        instance.deps.some((dependency) => dependency.mounted))
-    )
-  }
-  private updateMounted<S>(instance: Instance<S>) {
-    const mounted = this.isMounted(instance)
-    if (mounted === instance.mounted) return
-    instance.mounted = mounted
-    if (mounted) this.enqueueEffect(instance)
-    else this.unmountEffect(instance)
-    for (const sub of instance.subs) this.updateMounted(sub)
-  }
-  private updateValue<S, E>(
-    symbol: DerivedSymbol<S, E>,
-    instance: Instance<S>,
-  ) {
-    for (const dep of instance.deps) sortedRemove(dep.subs, instance)
-    instance.deps = []
-    const read = (<T, Key, Event>(
-      target: AtomFamily<T, Key, Event> | AtomSymbol<T, Event>,
-      key?: Key,
-    ) => {
-      const dep =
-        target.type === 'family'
-          ? this.getInstance(target, key!)
-          : this.getInstance(target)
-      sortedPush(instance.deps, dep)
-      sortedPush(dep.subs, instance)
-      return this.peekInstance(dep)
-    }) as Read
-    instance.value = symbol.getter(read)
-    this.updateMounted(instance)
-  }
-  private runEffect<S, E>(
-    symbol: AtomSymbol<S, E>,
-    instance: Instance<S>,
-    effect: Effect<S, E> | undefined,
-    next: S | undefined,
-  ) {
-    const last = instance.effectValue
-    instance.effectValue = next
-    instance.effectCleanup?.()
-    instance.effectCleanup =
-      effect?.(next, last, (event) => this.send(symbol, event)) || undefined
-  }
-  private unmountEffect<S, E>(instance: Instance<S>) {
-    const symbol = instance.symbol as AtomSymbol<S, E>
-    const { effect } = symbol
-    if (!effect) return
-    this.runEffect(symbol, instance, effect, undefined)
-  }
-  private transitionEffect<S, E>(instance: Instance<S>) {
-    const symbol: AtomSymbol<S, E> = instance.symbol
-    const next = this.peekInstance(instance)
-    if (!instance.mounted) return
-    if (Object.is(next, instance.effectValue)) return
-    this.runEffect(symbol, instance, symbol.effect, next)
-  }
-  private flush() {
-    const { primitives, effects } = this.tasks!
-    this.tasks = undefined
-    this.flushingEffects = effects
-    // oxlint-disable-next-line prefer-const
-    for (let [instance, next] of primitives)
-      if (!Object.is(next, instance.value)) {
-        instance.value = next
-        this.notify(instance)
-      }
-    this.flushingEffects = undefined
-    for (const instance of effects)
-      if (instance.mounted) this.transitionEffect(instance)
-  }
-  private peekInstance<S>(instance: Instance<S>) {
-    const { symbol } = instance
-    if (instance.dirty) {
-      instance.dirty = false
-      switch (symbol.type) {
-        case 'primitive':
-          instance.value = fromInit(symbol.getter)
-          return instance.value
-        case 'derived':
-          this.updateValue(symbol, instance)
-          return instance.value
-        default:
-          return exhaustive(symbol)
-      }
-    }
-    return instance.value
-  }
-  peek<S, Key, E>(symbol: AtomFamily<S, Key, E>, key: Key): S
-  peek<S, E>(symbol: AtomSymbol<S, E>): S
-  peek<S, Key, E>(symbol: AtomFamily<S, Key, E> | AtomSymbol<S, E>, key?: Key) {
-    const instance =
-      symbol.type === 'family'
-        ? this.getInstance(symbol, key!)
-        : this.getInstance(symbol)
-    return this.peekInstance(instance)
-  }
-  private sendInstance<S, E>(instance: Instance<S>, event: E) {
-    const { symbol } = instance
-    switch (symbol.type) {
-      case 'primitive': {
-        const { primitives } = this.getTasks()
-        let res: any
-        if (isFunction(event)) {
-          const last = primitives.has(instance)
-            ? primitives.get(instance)
-            : this.peekInstance(instance)
-          res = event(last)
-        } else if (isReset(event)) {
-          res = fromInit(symbol.getter)
-        } else {
-          res = event
-        }
-        primitives.set(instance, res)
-        return
-      }
-      case 'derived': {
-        symbol.setter(this.peek.bind(this), this.send.bind(this), event)
-        return
-      }
-      default:
-        return exhaustive(symbol)
-    }
-  }
-  send<S, Key, E>(symbol: AtomFamily<S, Key, E>, key: Key, next: E): void
-  send<S, E>(symbol: AtomSymbol<S, E>, next: E): void
-  send<S, Key, E>(
-    symbol: AtomFamily<S, Key, E> | AtomSymbol<S, E>,
-    keyOrNext: Key | E,
-    next?: E,
-  ): void {
-    const instance =
-      symbol.type === 'family'
-        ? this.getInstance(symbol, keyOrNext as Key)
-        : this.getInstance(symbol)
-    const event = symbol.type === 'family' ? next! : (keyOrNext as E)
-    this.sendInstance(instance, event)
-  }
-  private subscribeInstance<S>(instance: Instance<S>, notify: () => void) {
-    instance.subscriptions.add(notify)
-    this.updateMounted(instance)
-    return () => {
-      instance.subscriptions.delete(notify)
-      this.updateMounted(instance)
-    }
-  }
-  subscribe<S, Key, E>(
-    symbol: AtomFamily<S, Key, E>,
-    key: Key,
-    notify: () => void,
-  ): () => void
-  subscribe<S, E>(symbol: AtomSymbol<S, E>, notify: () => void): () => void
-  subscribe<S, Key, E>(
-    symbol: AtomFamily<S, Key, E> | AtomSymbol<S, E>,
-    keyOrNotify: Key | (() => void),
-    notify?: () => void,
-  ): () => void {
-    const instance =
-      symbol.type === 'family'
-        ? this.getInstance(symbol, keyOrNotify as Key)
-        : this.getInstance(symbol)
-    const callback =
-      symbol.type === 'family' ? notify! : (keyOrNotify as () => void)
-    return this.subscribeInstance(instance, callback)
-  }
+	private contents = new WeakMap<any, any>()
+	private tasks: Tasks | undefined = undefined
+	private flushingEffects: Set<Instance<any>> | undefined
+	constructor() {}
+	private getTasks(): Tasks {
+		if (this.tasks) return this.tasks
+		this.tasks = {
+			primitives: new Map(),
+			effects: new Set(),
+		}
+		queueMicrotask(this.flush.bind(this))
+		return this.tasks
+	}
+	private getInstance<S, Key, E>(
+		symbol: AtomFamily<S, Key, E> | AtomSymbol<S, E>,
+		key?: Key,
+	): Instance<S> {
+		if (symbol.type === 'family') {
+			let entry = this.contents.get(symbol)
+			if (entry === undefined) {
+				entry = new Map()
+				this.contents.set(symbol, entry)
+			}
+			let res = entry.get(key)
+			if (res === undefined) {
+				res = this.getInstance(symbol.create(key!))
+				entry.set(key, res)
+			}
+			return res
+		}
+		let res = this.contents.get(symbol)
+		if (res === undefined) {
+			res = this.createInstance(symbol)
+			this.contents.set(symbol, res)
+		}
+		return res
+	}
+	private createInstance<S, E>(symbol: AtomSymbol<S, E>): Instance<S> {
+		return {
+			symbol,
+			dirty: symbol.type === 'derived',
+			value:
+				symbol.type === 'primitive'
+					? fromInit(symbol.getter)
+					: (undefined as any),
+			effectValue: undefined,
+			effectCleanup: undefined,
+			deps: [],
+			subs: [],
+			subscriptions: new Set(),
+			mounted: false,
+		}
+	}
+	private enqueueEffect<S>(instance: Instance<S>) {
+		if (!instance.symbol.effect) return
+		if (this.flushingEffects) {
+			this.flushingEffects.add(instance)
+			return
+		}
+		this.getTasks().effects.add(instance)
+	}
+	private notify<S>(instance: Instance<S>) {
+		if (instance.symbol.type === 'derived') instance.dirty = true
+		if (instance.mounted) this.enqueueEffect(instance)
+		for (const sub of instance.subs) this.notify(sub)
+		instance.subscriptions.forEach(call)
+	}
+	private isMounted<S>(instance: Instance<S>) {
+		return (
+			instance.subscriptions.size > 0 ||
+			(instance.symbol.type === 'derived' &&
+				instance.deps.some((dependency) => dependency.mounted))
+		)
+	}
+	private updateMounted<S>(instance: Instance<S>) {
+		const mounted = this.isMounted(instance)
+		if (mounted === instance.mounted) return
+		instance.mounted = mounted
+		if (mounted) this.enqueueEffect(instance)
+		else this.unmountEffect(instance)
+		for (const sub of instance.subs) this.updateMounted(sub)
+	}
+	private runEffect<S, E>(
+		symbol: AtomSymbol<S, E>,
+		instance: Instance<S>,
+		effect: Effect<S, E> | undefined,
+		next: S | undefined,
+	) {
+		const last = instance.effectValue
+		instance.effectValue = next
+		instance.effectCleanup?.()
+		instance.effectCleanup =
+			effect?.(next, last, (event) => this.send(symbol, event)) || undefined
+	}
+	private unmountEffect<S, E>(instance: Instance<S>) {
+		const symbol = instance.symbol as AtomSymbol<S, E>
+		const { effect } = symbol
+		if (!effect) return
+		this.runEffect(symbol, instance, effect, undefined)
+	}
+	private transitionEffect<S, E>(instance: Instance<S>) {
+		const symbol: AtomSymbol<S, E> = instance.symbol
+		const next = this.peekInstance(instance)
+		if (!instance.mounted) return
+		if (Object.is(next, instance.effectValue)) return
+		this.runEffect(symbol, instance, symbol.effect, next)
+	}
+	private flush() {
+		const { primitives, effects } = this.tasks!
+		this.tasks = undefined
+		this.flushingEffects = effects
+		// oxlint-disable-next-line prefer-const
+		for (let [instance, next] of primitives)
+			if (!Object.is(next, instance.value)) {
+				instance.value = next
+				this.notify(instance)
+			}
+		this.flushingEffects = undefined
+		for (const instance of effects)
+			if (instance.mounted) this.transitionEffect(instance)
+	}
+	private peekInstance<S>(instance: Instance<S>) {
+		const { symbol } = instance
+		if (instance.dirty) {
+			instance.dirty = false
+			switch (symbol.type) {
+				case 'primitive':
+					instance.value = fromInit(symbol.getter)
+					return instance.value
+				case 'derived': {
+					for (const dep of instance.deps) sortedRemove(dep.subs, instance)
+					instance.deps = []
+					const read = (<T, Key, Event>(
+						target: AtomFamily<T, Key, Event> | AtomSymbol<T, Event>,
+						key?: Key,
+					) => {
+						const dep = this.getInstance(target, key)
+						sortedPush(instance.deps, dep)
+						sortedPush(dep.subs, instance)
+						return this.peekInstance(dep)
+					}) as Read
+					instance.value = symbol.getter(read)
+					this.updateMounted(instance)
+					return instance.value
+				}
+				default:
+					return exhaustive(symbol)
+			}
+		}
+		return instance.value
+	}
+	peek<S, Key, E>(symbol: AtomFamily<S, Key, E>, key: Key): S
+	peek<S, E>(symbol: AtomSymbol<S, E>): S
+	peek<S, Key, E>(symbol: AtomFamily<S, Key, E> | AtomSymbol<S, E>, key?: Key) {
+		const instance = this.getInstance(symbol, key)
+		return this.peekInstance(instance)
+	}
+	private sendInstance<S, E>(instance: Instance<S>, event: E) {
+		const { symbol } = instance
+		switch (symbol.type) {
+			case 'primitive': {
+				const { primitives } = this.getTasks()
+				let res: any
+				if (isFunction(event)) {
+					const last = primitives.has(instance)
+						? primitives.get(instance)
+						: this.peekInstance(instance)
+					res = event(last)
+				} else if (isReset(event)) {
+					res = fromInit(symbol.getter)
+				} else {
+					res = event
+				}
+				primitives.set(instance, res)
+				return
+			}
+			case 'derived': {
+				symbol.setter(this.peek.bind(this), this.send.bind(this), event)
+				return
+			}
+			default:
+				return exhaustive(symbol)
+		}
+	}
+	send<S, Key, E>(symbol: AtomFamily<S, Key, E>, key: Key, next: E): void
+	send<S, E>(symbol: AtomSymbol<S, E>, next: E): void
+	send<S, Key, E>(
+		symbol: AtomFamily<S, Key, E> | AtomSymbol<S, E>,
+		keyOrNext: Key | E,
+		next?: E,
+	): void {
+		const instance = this.getInstance(symbol, keyOrNext as any)
+		const event = symbol.type === 'family' ? next! : (keyOrNext as E)
+		this.sendInstance(instance, event)
+	}
+	private subscribeInstance<S>(instance: Instance<S>, notify: () => void) {
+		instance.subscriptions.add(notify)
+		this.updateMounted(instance)
+		return () => {
+			instance.subscriptions.delete(notify)
+			this.updateMounted(instance)
+		}
+	}
+	subscribe<S, Key, E>(
+		symbol: AtomFamily<S, Key, E>,
+		key: Key,
+		notify: () => void,
+	): () => void
+	subscribe<S, E>(symbol: AtomSymbol<S, E>, notify: () => void): () => void
+	subscribe<S, Key, E>(
+		symbol: AtomFamily<S, Key, E> | AtomSymbol<S, E>,
+		keyOrNotify: Key | (() => void),
+		notify?: () => void,
+	): () => void {
+		const instance = this.getInstance(symbol, keyOrNotify as Key)
+		const callback =
+			symbol.type === 'family' ? notify! : (keyOrNotify as () => void)
+		return this.subscribeInstance(instance, callback)
+	}
 }
